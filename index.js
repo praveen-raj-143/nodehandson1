76e08aa5948f15023f17e5852d4c99dd6736272c
praveen-raj-143 <praveenrajgreath@gmail.com>
2023-03-27 21:59:58 +0530

    modified

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const http=require("http");
 http.createServer(function(req,res){
     if(req.url === '/'){
         res.writeHead(200,{'Content-Type':'text/html'});
-        res.write('<h1>Hello I am default page</h1>');
+        res.write('<h1>what is node ?</h1>');
+        res.write('<h2>Node allows developers to write JavaScript code that runs directly in a computer process itself instead of in a browser. Node and Nodejs is same.</h2>')
+        res.write('<h2>Node is a cross-platform, open-source JavaScript runtime environment that allows developers to run JavaScript code outside of the web browser.</h2>')
+        res.write('<ul><li>Node is an open source server environment.</li><li>Node is free</li><li>Node runs on various platforms(Windows, Linux, Unix, Mac OS X, etc.)</li><li>Node uses JavaScript on the server</li></ul>')
         res.end();
     }
     else if(req.url==="/home"){
